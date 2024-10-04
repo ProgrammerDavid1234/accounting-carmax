af9325deab4a7c222c1ef420d50eb487b9043d86
ProgrammerDavid1234 <olonadenifemi@gmail.com>
2024-10-04 10:09:34 +0100

    Resolved merge conflicts

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,6 @@
 const express = require('express');
-
 const cors = require('cors');
 const compression = require('compression');
-
 const cookieParser = require('cookie-parser');
 
 const coreAuthRouter = require('./routes/coreRoutes/coreAuth');
@@ -15,13 +13,16 @@ const errorHandlers = require('./handlers/errorHandlers');
 const erpApiRouter = require('./routes/appRoutes/appApi');
 
 const fileUpload = require('express-fileupload');
-// create our Express app
+
+// Create our Express app
 const app = express();
 
+// CORS Configuration
 app.use(
   cors({
-    origin: true,
-    credentials: true,
+    origin: ['https://carmax-one.vercel.app'], // Frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // If your frontend is sending cookies or using sessions
   })
 );
 
@@ -31,22 +32,18 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(compression());
 
-// // default options
-// app.use(fileUpload());
-
 // Here our API Routes
-
 app.use('/api', coreAuthRouter);
 app.use('/api', adminAuth.isValidAuthToken, coreApiRouter);
 app.use('/api', adminAuth.isValidAuthToken, erpApiRouter);
 app.use('/download', coreDownloadRouter);
 app.use('/public', corePublicRouter);
 
-// If that above routes didnt work, we 404 them and forward to error handler
+// If the above routes didn’t work, we 404 them and forward to error handler
 app.use(errorHandlers.notFound);
 
-// production error handler
+// Production error handler
 app.use(errorHandlers.productionErrors);
 
-// done! we export it so we can start the site in start.js
+// Done! We export it so we can start the site in start.js
 module.exports = app;
