cf4f372ce173097773026abe84fde4b52f7c4dc0
ProgrammerDavid1234 <olonadenifemi@gmail.com>
2024-10-17 18:04:17 +0100

    Admin's Pages completed

--- frontend/src/apps/IdurarOs.jsx
+++ frontend/src/apps/IdurarOs.jsx
@@ -1,5 +1,5 @@
+import { App } from 'antd';
 import { lazy, Suspense, useEffect, useState } from 'react';
-
 import { useSelector } from 'react-redux';
 import { selectAuth } from '@/redux/auth/selectors';
 import { AppContextProvider } from '@/context/appContext';
@@ -13,9 +13,11 @@ const ErpApp = lazy(() => import('./ErpApp'));
 const DefaultApp = () => (
   <Localization>
     <AppContextProvider>
-      <Suspense fallback={<PageLoader />}>
-        <ErpApp />
-      </Suspense>
+      <App> {/* Wrapping with Ant Design's App */}
+        <Suspense fallback={<PageLoader />}>
+          <ErpApp />
+        </Suspense>
+      </App>
     </AppContextProvider>
   </Localization>
 );
@@ -27,39 +29,35 @@ export default function IdurarOs() {
     '🚀 Welcome to IDURAR ERP CRM! Did you know that we also offer commercial customization services? Contact us at hello@idurarapp.com for more information.'
   );
 
-  // // Online state
-  // const [isOnline, setIsOnline] = useState(navigator.onLine);
+  // Online state
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
 
-  // useEffect(() => {
-  //   // Update network status
-  //   const handleStatusChange = () => {
-  //     setIsOnline(navigator.onLine);
-  //     if (!isOnline) {
-  //       console.log('🚀 ~ useEffect ~ navigator.onLine:', navigator.onLine);
-  //       notification.config({
-  //         duration: 20,
-  //         maxCount: 1,
-  //       });
-  //       // Code to execute when there is internet connection
-  //       notification.error({
-  //         message: 'No internet connection',
-  //         description: 'Cannot connect to the Internet, Check your internet network',
-  //       });
-  //     }
-  //   };
+  useEffect(() => {
+    // Update network status
+    const handleStatusChange = () => {
+      setIsOnline(navigator.onLine);
+      if (!navigator.onLine) {
+        notification.config({
+          duration: 20,
+          maxCount: 1,
+        });
+        notification.error({
+          message: 'No internet connection',
+          description: 'Cannot connect to the Internet, Check your internet network',
+        });
+      }
+    };
 
-  //   // Listen to the online status
-  //   window.addEventListener('online', handleStatusChange);
+    // Listen to the online/offline events
+    window.addEventListener('online', handleStatusChange);
+    window.addEventListener('offline', handleStatusChange);
 
-  //   // Listen to the offline status
-  //   window.addEventListener('offline', handleStatusChange);
-
-  //   // Specify how to clean up after this effect for performance improvment
-  //   return () => {
-  //     window.removeEventListener('online', handleStatusChange);
-  //     window.removeEventListener('offline', handleStatusChange);
-  //   };
-  // }, [navigator.onLine]);
+    // Cleanup the event listeners
+    return () => {
+      window.removeEventListener('online', handleStatusChange);
+      window.removeEventListener('offline', handleStatusChange);
+    };
+  }, [isOnline]);
 
   if (!isLoggedIn)
     return (
